de8118e53470c8d37746aab4cea956497f4d2f16
Evgeny Gusev <wackyy52@gmail.com>
2020-02-13 15:11:58 +0300

    Add Delegates

--- Practice2/ViewController.swift
+++ Practice2/ViewController.swift
@@ -14,9 +14,53 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        starWarsSearchBar.delegate = self
+        
+        let starWarsCellNib = UINib(nibName: "SearchTableViewCell", bundle: nil)
+        
+        resultSearchTableView.register(starWarsCellNib, forCellReuseIdentifier: "SearchTableViewCell")
+        resultSearchTableView.delegate = self
+        resultSearchTableView.dataSource = self
     }
+}
 
+extension ViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText:
+        String) {
+        resultSearchTableView.reloadData()
+        
+    }
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        
+    }
+    
+}
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if(section == 0) {
+            return "Search"
+        }
+        
+        return nil
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: "SearchTableViewCell", for: indexPath)
+            as? SearchTableViewCell else { return UITableViewCell() }
+        
+        cell.prepareForAppear(text: "Bob")
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    }
+    
 }
 
