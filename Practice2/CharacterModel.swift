bd172f5e75b41ad5ec1c294214384935360f4f8c
Evgeny Gusev <wackyy52@gmail.com>
2020-02-14 13:14:50 +0300

    Add new key for decode

--- Practice2/CharacterModel.swift
+++ Practice2/CharacterModel.swift
@@ -10,14 +10,36 @@ import Foundation
 
 struct Character: Codable {
     let name: String
+    let height: String
+    let mass: String
+    let hairColor: String
+    let skinColor: String
+    let eyeColor: String
+    let birthYear: String
+    let gender: String
     
     enum CodingKeys: String, CodingKey {
-            case name = "name"
+        case name = "name"
+        case height = "height"
+        case mass = "mass"
+        case hairColor = "hair_color"
+        case skinColor = "skin_color"
+        case eyeColor = "eye_color"
+        case birthYear = "birth_year"
+        case gender = "gender"
     }
     
     init(from decoder: Decoder) throws {
         let valueContainer = try decoder.container(keyedBy: CodingKeys.self)
+        
         self.name = try valueContainer.decode(String.self, forKey: CodingKeys.name)
+        self.height = try valueContainer.decode(String.self, forKey: CodingKeys.height)
+        self.mass = try valueContainer.decode(String.self, forKey: CodingKeys.mass)
+        self.hairColor = try valueContainer.decode(String.self, forKey: CodingKeys.hairColor)
+        self.skinColor = try valueContainer.decode(String.self, forKey: CodingKeys.skinColor)
+        self.eyeColor = try valueContainer.decode(String.self, forKey: CodingKeys.eyeColor)
+        self.birthYear = try valueContainer.decode(String.self, forKey: CodingKeys.birthYear)
+        self.gender = try valueContainer.decode(String.self, forKey: CodingKeys.gender)
     }
-
 }
+
